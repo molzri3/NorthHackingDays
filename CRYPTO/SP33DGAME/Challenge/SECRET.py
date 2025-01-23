2b8512190e64401aad2804e10d019ba1dfbd7886
Ridiwane MAMA TOURE <ridiwanemamatoure@gmail.com>
2025-01-23 22:53:51 +0100

    Update SECRET.py

--- CRYPTO/SP33DGAME/Challenge/SECRET.py
+++ CRYPTO/SP33DGAME/Challenge/SECRET.py
@@ -1,7 +1,7 @@
-FLAG=b"CTF{th1s_1s_th3_re4l17y_th3_d0nt_w4nt_y0u_t0_know}"
+FLAG=b"CTF{th15_15_7h3_r34l17y_7h3_d0n7_w4n7_y0u_70_know}"
 banner=r'''
                                    __    
 (\,---------------'()'--o
  (_    _sp33game_    /~" 
-  (_)_)         (_)_)                    ¯\_(ツ)_/¯    Neo choose the truth  ¯\_(ツ)_/¯
+  (_)_)         (_)_)                    ¯\_(ツ)_/¯ you deserve to know the truth ¯\_(ツ)_/¯
 '''
