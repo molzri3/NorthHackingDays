3d8cf22ec482e58b9e8c69f2ceaa4843a494302f
stoic-crawler <priiiivateteeeestaccccount@gmail.com>
2025-02-03 02:54:38 +0100

    Create vuln.c

---
+++ PWN/Classic/vuln.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+ 
+#define FLAGSIZE_MAX 64
+ 
+char flag[FLAGSIZE_MAX];
+ 
+void sigsegv_handler(int sig) {
+    printf("**********Good_Job**********");
+    fflush(stdout);
+    printf("%s", flag);
+    fflush(stdout);
+    exit(3);
+}
+ 
+void vuln(){
+        char buf1[333];
+	printf("MY_Plate: ");
+	fflush(stdout);
+	gets(buf1); 
+	printf("bruuh! feed me more ......\n");
+  
+
+}
+ 
+int main(int argc, char **argv){
+    
+    FILE *f = fopen("/flag.txt","r");
+    if (f == NULL) {
+        printf("%s %s", "Please create 'flag.txt' in this directory with your",
+                        "own debugging flag.\n");
+        exit(0);
+    }
+    
+    fgets(flag,FLAGSIZE_MAX,f);
+    signal(SIGSEGV, sigsegv_handler); // Set up signal handler
+    
+    
+    while(1){
+    	vuln();
+    }
+    return 0;
+}
