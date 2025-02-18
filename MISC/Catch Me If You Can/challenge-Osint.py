6da97ba6b546b11c13ed1b5aed891813e3276905
F1Z3R <elhassany.alae@etu.uae.ac.ma>
2025-02-19 00:48:29 +0100

    Update challenge-Osint.py

--- MISC/Catch Me If You Can/challenge-Osint.py
+++ MISC/Catch Me If You Can/challenge-Osint.py
@@ -2,14 +2,13 @@ from PIL import Image
 import random
 
 def encode_image(image_path, text, key):
-    img = Image.open(image_path).convert("RGB")  # Ensure image is in RGB mode
+    img = Image.open(image_path).convert("RGB")  
     binary_data = ''.join(format(ord(i), '08b') for i in text)
 
     img_data = img.load()
     width, height = img.size
     data_index = 0
 
-    # Use the key to seed the random number generator
     random.seed(key)
     pixel_order = list(range(width * height))
     random.shuffle(pixel_order)
@@ -19,9 +18,9 @@ def encode_image(image_path, text, key):
             break
         x = pixel_index % width
         y = pixel_index // width
-        r, g, b = img_data[x, y]  # Extract RGB values
-        new_b = (b & 0xFE) | int(binary_data[data_index])  # Modify LSB of blue channel
-        img_data[x, y] = (r, g, new_b)  # Update pixel
+        r, g, b = img_data[x, y]  
+        new_b = (b & 0xFE) | int(binary_data[data_index]) 
+        img_data[x, y] = (r, g, new_b)  
         data_index += 1
 
     img.save("steg.png")
