f7d4ada61564ebc07dc774e430864330a49e0e6c
med-bt <146647504+med-bt@users.noreply.github.com>
2025-02-19 15:22:14 +0100

    Update sol.py

--- CRYPTO/MARTIX_RSA/solution/sol.py
+++ CRYPTO/MARTIX_RSA/solution/sol.py
@@ -12,7 +12,6 @@ def decrypt_flag(ciphertext, matrix):
         raise Exception("Matrix is not invertible modulo 256 (determinant has no inverse)")
     
     adjugate = np.round(np.linalg.inv(matrix) * det).astype(np.int64)
-    
     matrix_inv = (adjugate * det_inv) % 256
     print("[Debug] Inverse Matrix:\n", matrix_inv)
     
@@ -21,27 +20,25 @@ def decrypt_flag(ciphertext, matrix):
     decrypted_bytes = decrypted.astype(np.uint8).flatten().tobytes()
     
     print("[Debug] Decrypted bytes (hex):", decrypted_bytes.hex())
-    
     clean_bytes = decrypted_bytes.rstrip(b'\x00')
     return clean_bytes.decode('latin-1')
 
 def main():
-    with open("output.txt", "rb") as f:
-        data = f.read().split(b"\n\nRSA_N=")
-    
-    ciphertext = data[0]
-    params = data[1].decode().split('\n')
-    
-    N = int(params[0])
-    e = int(params[1].split('=')[1])
-    rsa_matrix = list(map(int, params[2].split('=')[1].split(',')))
+    with open("solution\output.txt", "r") as f:
+        lines = [line.strip() for line in f if line.strip()]
 
-    # prime factors (player should factor N)
+    ciphertext_hex = lines[0]
+    ciphertext = bytes.fromhex(ciphertext_hex)
+
+    N = int(lines[1].split('=')[1])
+    e = int(lines[2].split('=')[1])
+    matrix_values = list(map(int, lines[3].split('=')[1].split(',')))
     p = 123456789101112167
     q = 123456789101112109
-    phi = (p-1) * (q-1)
+    phi = (p - 1) * (q - 1)
     d = inverse(e, phi)
-    secret_values = [pow(c, d, N) % 256 for c in rsa_matrix]
+    
+    secret_values = [pow(c, d, N) % 256 for c in matrix_values]
     secret_matrix = np.array(secret_values, dtype=np.uint8).reshape(3, 3)
     print("[Debug] Decrypted Secret Matrix:\n", secret_matrix)
     
